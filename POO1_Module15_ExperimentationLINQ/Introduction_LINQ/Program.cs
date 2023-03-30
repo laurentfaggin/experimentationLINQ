94aa8ebcc284ac99467d9aa8293814beddcabf11
laurentfaggin <112583742+laurentfaggin@users.noreply.github.com>
2023-03-30 18:20:19 -0400

    Add files via upload

---
+++ POO1_Module15_ExperimentationLINQ/Introduction_LINQ/Program.cs
@@ -0,0 +1,184 @@
+﻿using ExperimentationsLINQ;
+
+namespace Introduction_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ObtenirElement01_SyntaxeRequete_v01();
+            //ObtenirELement01_SyntaxeMethode_v01();
+            //ObtenirElement01_SyntaxeRequete_v02();
+            //ObtenirElement01_SyntaxeRequete_v02Bis();
+            //ObtenirElement01_SyntaxeMethode_v02Bis();
+            //ObtenirElement01_SyntaxeMethode_v02();
+            //ObtenirListeNomsProduits_SyntaxeRequete_v01();
+            //ObtenirListeNomsProduits_SyntaxeMethode_v01();
+            //ObtenirListeCategories_SyntaxeRequete_v01();
+            //ObtenirListeCategories_SyntaxeMethode_v01();
+            //ObtenirListeCategories_SyntaxeRequete_v02();
+            //ObtenirListeCategories_SyntaxeMethode_v02();
+            //ObtenirListeNomsProduitsOrdonnee_SyntaxeRequete_v01();
+            //ObtenirListeNomsProduitsOrdonnee_SyntaxeMethode_v01();
+            ObtenirListeCategoriesOrdonnee_SyntaxeRequete_v01();
+            ObtenirListeCategoriesOrdonnee_SyntaxeMethode_v01();
+        }
+
+        /*****************             Filtrer une collection          ***********/
+        /********* element unique  ***********/
+        /*******  requête   ********/
+        private static void ObtenirElement01_SyntaxeRequete_v01()
+        {
+         var produits = DonneesLINQ.CreateProductList();
+         var resultat = from produit in produits
+                        where produit.ProductID == 12
+                        select produit;
+         ObjectDumper.Write(resultat);
+        }
+
+        /**********   methode   *******/
+        private static void ObtenirELement01_SyntaxeMethode_v01()
+        {
+         var produits = DonneesLINQ.CreateProductList();
+         var resultat = produits.Where(p => p.ProductID == 12);
+
+         ObjectDumper.Write(resultat);
+        }
+
+        /*******  requête   ********/
+        private static void ObtenirElement01_SyntaxeRequete_v02()
+        {
+         var produits = DonneesLINQ.CreateProductList();
+         var resultat = (from produit in produits
+                         where produit.ProductID == 12
+                         select produit).Single();
+
+         ObjectDumper.Write(resultat);
+        }
+
+        private static void ObtenirElement01_SyntaxeRequete_v02Bis()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var resultat = (from produit in produits
+                            where produit.ProductID == 12
+                            select produit).SingleOrDefault();
+
+            ObjectDumper.Write(resultat);
+        }
+
+        /**********   methode   *******/
+        private static void ObtenirElement01_SyntaxeMethode_v02()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var resultat = produits.Where(p => p.ProductID == 12).SingleOrDefault();
+
+            ObjectDumper.Write(resultat);
+        }
+
+        /*******  methode   ********/
+        private static void ObtenirElement01_SyntaxeMethode_v02Bis()
+        {
+         var produits = DonneesLINQ.CreateProductList();
+         var resultat = produits.Where(p => p.ProductID == 12).Single();
+
+         ObjectDumper.Write(resultat);
+        }
+
+        /**************  afficher produit  **************/
+                
+        private static void ObtenirListeNomsProduits_SyntaxeRequete_v01()
+        {
+            var Produits = DonneesLINQ.CreateProductList();
+            var categories = from produit in Produits
+                             select produit.ProductName;
+
+            ObjectDumper.Write(categories);
+        }
+
+        private static void ObtenirListeNomsProduits_SyntaxeMethode_v01()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var categories = produits.Select(p => p.ProductName);
+
+            ObjectDumper.Write(categories);
+        }
+
+        /*********************  afficher categories   *****************/
+
+        private static void ObtenirListeCategories_SyntaxeRequete_v01()
+        {
+            var Produits = DonneesLINQ.CreateProductList();
+            var categories = from produit in Produits
+                             select produit.Category;
+
+            ObjectDumper.Write(categories);
+        }
+
+        private static void ObtenirListeCategories_SyntaxeMethode_v01()
+        {
+            var Produits = DonneesLINQ.CreateProductList();
+            var categories = Produits.Select(p  =>  p.Category);
+
+            ObjectDumper.Write(categories);
+        }
+
+        //la methode et la requete au dessus retourne des doublons.
+
+        private static void ObtenirListeCategories_SyntaxeRequete_v02()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var categories = (from produit in produits
+                              select produit.Category).Distinct();
+
+            ObjectDumper.Write(categories);
+        }
+
+        private static void ObtenirListeCategories_SyntaxeMethode_v02()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var categories = produits.Select(p => p.Category).Distinct();
+
+            ObjectDumper.Write(categories);
+        }
+
+        //la methode et la requete du dessus retourne les categories en supprimant les doublons.
+
+        /*****************    TRI     *****************/
+
+        private static void ObtenirListeNomsProduitsOrdonnee_SyntaxeRequete_v01()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var names = from produit in produits
+                             orderby produit.ProductName
+                             select produit.ProductName;
+
+            ObjectDumper.Write(names);
+        }
+
+        private static void ObtenirListeNomsProduitsOrdonnee_SyntaxeMethode_v01()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            //var names = produits.OrderBy(p => p.ProductName).Select(p => p.ProductName);
+            var names2 = produits.Select (p => p.ProductName).OrderBy (c => c);
+
+            ObjectDumper.Write(names2);
+        }
+
+        private static void ObtenirListeCategoriesOrdonnee_SyntaxeRequete_v01()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var categories = from produit in produits
+                             orderby produit.Category
+                             select produit.Category;
+            ObjectDumper.Write(categories);
+        }
+
+        private static void ObtenirListeCategoriesOrdonnee_SyntaxeMethode_v01()
+        {
+            var produits = DonneesLINQ.CreateProductList();
+            var categories = produits.OrderBy(p => p.Category).Select(p => p.Category).Distinct(); 
+
+            ObjectDumper.Write(categories);
+        }
+    }
+}
---
+++ POO1_Module15_ExperimentationLINQ/POO1_Module15_ExperimentationLINQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExperimentationsLINQ;
+
+namespace POO1_Module15_ExperimentationLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Func<int, int> fctIdentite_V01 = (int x) => x;
+        //    ObjectDumper.Write(Identite(10));
+
+        //    Func <double, double> fctCarre_V01 = (double y) => y * y;
+        //    ObjectDumper.Write(fctCarre_V01(5));
+        
+            Func<int, int, int> fctMultiplication_V01 = (x, y) => x * y;
+            ObjectDumper.Write(fctMultiplication_V01(5, 3));
+            
+            Func<int, int> fctIdentite_V02 = (x) => x;
+            ObjectDumper.Write(fctIdentite_V02(10));
+
+            Func<int, int, int> fctAddition_V02 = (x, y) => x + y;
+            ObjectDumper.Write(fctAddition_V02(10, 20));
+
+
+        }
+
+        private static int Identite(int val)
+        {
+            return val;
+        }
+
+        public double Carre(double p_chiffre)
+        {
+            return p_chiffre * p_chiffre;
+        }
+
+        public int Multiplicatiom(int p_valeur1, int p_valeur2)
+        {
+            return p_valeur1 * p_valeur2;
+        }
+    }
+}
